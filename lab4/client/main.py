dfcf659e4ea7cf9fb99850cb42c905a9b37864c0
bio_tngiabao <bao.thai@bioturing.com>
2025-11-27 21:49:20 +0700

    Refactor gRPC client implementation by creating a dedicated GRPCClient class, removing the previous command stream logic from main.py, and enhancing error handling.

--- lab4/client/main.py
+++ lab4/client/main.py
@@ -1,6 +1,6 @@
 import grpc
 import time
-import subprocess
+
 
 from generated.monitor_pb2_grpc import MonitorServiceStub
 from generated.monitor_pb2 import CommandResponse
@@ -8,54 +8,12 @@ import socket
 import datetime
 from constant import MetricType
 from module.plugins.manager import PlugingManager
-from module.plugins._base import BasePlugin
-import logging
-
-plugin_manager = PlugingManager()
-plugin_manager.load_plugins()
-
-last_metric = MetricType.CPU
-
-
-def command_stream():
-    current_metric = MetricType.CPU
-
-    while True:
-        plugin = plugin_manager.get_plugin(current_metric)
-
-        value = "Metric not found"
-        if plugin is not None:
-            value = plugin.run()
-            if value is None:
-                value = "Cannot get metric value"
-            else:
-                value = str(value)
-
-        yield CommandResponse(
-            timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            hostname=socket.gethostname(),
-            metric=current_metric,
-            value=value,
-        )
-
-        time.sleep(1)
-
-        if current_metric != last_metric:
-            current_metric = last_metric
+from module.grpc import GRPCClient
 
 
 def main():
-    channel = grpc.insecure_channel("localhost:50051")
-    stub = MonitorServiceStub(channel)
-
-    responses = stub.CommandStream(command_stream())
-    try:
-        for response in responses:
-            print(f"Received command: {response.command}")
-            global last_metric
-            last_metric = response.command
-    except grpc.RpcError as e:
-        print(f"RPC error: {e.code()} - {e.details()}")
+    grpc_client = GRPCClient(address="localhost:50051", plugin_manager=PlugingManager())
+    grpc_client.run()
 
 
 if __name__ == "__main__":
---
+++ lab4/client/module/grpc.py
@@ -0,0 +1,63 @@
+import grpc
+from generated.monitor_pb2_grpc import MonitorServiceStub
+from generated.monitor_pb2 import CommandRequest, CommandResponse
+from constant import MetricType
+import datetime
+import socket
+import time
+
+from module.plugins.manager import PlugingManager
+from module.plugins._base import BasePlugin
+import logging
+
+
+class GRPCClient:
+    def __init__(self, address: str, plugin_manager: PlugingManager):
+        self.channel = grpc.insecure_channel(address)
+        self.stub = MonitorServiceStub(self.channel)
+        self.last_command = MetricType.CPU
+
+        self.plugin_manager = plugin_manager
+        self.plugin_manager.load_plugins()
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.info(f"GRPCClient initialized with address: {address}")
+
+    def run(self):
+        responses = self.stub.CommandStream(self.command_stream())
+        try:
+            for response in responses:
+                response: CommandRequest
+                self.logger.info(f"Received command: {response.command}")
+
+                self.last_command = response.command
+        except grpc.RpcError as e:
+            self.logger.error(f"RPC error: {e.code()} - {e.details()}")
+        except Exception as e:
+            self.logger.error(f"Error: {e}")
+
+    def command_stream(self):
+        current_metric = self.last_command
+
+        while True:
+            plugin = self.plugin_manager.get_plugin(current_metric)
+
+            value = "Metric not found"
+            if plugin is not None:
+                value = plugin.run()
+                if value is None:
+                    value = "Cannot get metric value"
+                else:
+                    value = str(value)
+
+            yield CommandResponse(
+                timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                hostname=socket.gethostname(),
+                metric=current_metric,
+                value=value,
+            )
+
+            time.sleep(1)
+
+            if current_metric != self.last_command:
+                current_metric = self.last_command
