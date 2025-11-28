7e0a38776e03c5383d9bdb07f3a700949fe5e701
tngiabao <bao.thai@bioturing.com>
2025-11-28 16:07:12 +0700

    Increase sleep duration in command stream to improve metric retrieval timing; update requirements.txt for new dependency

--- lab4/client/module/grpc_client.py
+++ lab4/client/module/grpc_client.py
@@ -58,7 +58,7 @@ class GRPCClient:
                 unit=plugin.unit if plugin else "N/A",
             )
 
-            time.sleep(0.01)
+            time.sleep(0.2)
 
             if current_metric != self.last_command:
                 current_metric = self.last_command
